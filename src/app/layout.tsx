6d790091dafe4544569efc511692048492c556c8
DTrombett <maxtromb.dt@gmail.com>
2023-11-08 18:33:04 +0100

    feat: use display swap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-const font = Noto_Sans({ weight: "400", subsets: ["latin"] });
+const font = Noto_Sans({ weight: "400", subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
